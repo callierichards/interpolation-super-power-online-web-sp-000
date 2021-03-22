f0336e8efecb1e308d19c89d043a3a40ae192a8a
callie rogers <callie1013@gmail.com>
2021-03-22 17:35:03 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,11 +4,11 @@ letters = ['R','O','Y','G','B','I','V']
 display_rainbow = ['red','orange','yellow','green','blue','indigo','violet']
 
 def display_rainbow.array (colors)
-  puts "R: #{colors[0]},"
-  puts "O: #{colors[1]},"
-  puts "Y: #{colors[2]},"
-  puts "G: #{colors[3]},"
-  puts "B: #{colors[4]},"
-  puts "I: #{colors[5]},"
-  puts "V: #{colors[6]},"
+  puts "R: #{colors[0]}"
+  puts "O: #{colors[1]}"
+  puts "Y: #{colors[2]}"
+  puts "G: #{colors[3]}"
+  puts "B: #{colors[4]}"
+  puts "I: #{colors[5]}"
+  puts "V: #{colors[6]}"
 end
